ec62b7c72386664da1c0574b70b1f58e50e424cc
Claveaum <maxime.claveau@zenika.com>
2018-03-01 19:53:46 +0100

    add distance func

--- solve.js
+++ solve.js
@@ -1,29 +1,30 @@
-const debug = require("debug")("solve");
-const _ = require("lodash");
-const gridUtils = require("./grid-utils");
+const debug = require('debug')('solve')
+const _ = require('lodash')
+const gridUtils = require('./grid-utils')
+const distance = require('./distance')
 
 function solve(problem) {
   // destructure this!
-  const { rides, nvehicules } = problem;
+  const { rides, nvehicules } = problem
 
-  let count = 0;
+  let count = 0
 
-  const result = {};
+  const result = {}
 
   rides.forEach((ride, index) => {
     if (index % nvehicules === 0) {
-      count = 0;
+      count = 0
     } else {
-      count++;
+      count++
     }
 
-    result[count] = !result[count] ? [index] : [...result[count], index];
-  });
+    result[count] = !result[count] ? [index] : [...result[count], index]
+  })
 
-  console.log(result);
+  console.log(result)
   return _.map(result, ridesPerVehicle => {
-    return `${ridesPerVehicle.length} ${ridesPerVehicle.join(" ")}`;
-  });
+    return `${ridesPerVehicle.length} ${ridesPerVehicle.join(' ')}`
+  })
 }
 
-module.exports = solve;
+module.exports = solve
