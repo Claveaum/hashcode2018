bc7adc4cf0ba1aa95d53d3246657b5bad6d2e61c
Charles-Henri Guérin <charles-henri.guerin@zenika.com>
2018-03-01 20:31:22 +0100

    fuck

--- solve.js
+++ solve.js
@@ -15,26 +15,33 @@ function solve(problem) {
 
   const copyRides = [...rides]
 
-  for (let index = 0; copyRides.length; ) {
-    if (index % nvehicules === 0) {
-      count = 0
-    } else {
-      count++
+  let index = 0
+  try {
+    for (; copyRides.length; ) {
+      if (index === 8826) { console.log(index, count) }
+      if (index === 8827) { console.log(index, count, index % nvehicules) }
+      if (index % nvehicules === 0) {
+        count = 0
+      } else {
+        count++
+      }
+
+      const { ox, oy, dx, dy } = rides[index]
+      const rideDistance = distance(ox, oy, dx, dy)
+      const vehiculeRides = stack[count]
+
+      const somme = vehiculeRides.reduce((sum , rideIndex) => {
+        const tasoeur = rides[rideIndex]
+        return sum + distance(tasoeur.ox, tasoeur.oy, tasoeur.dx, tasoeur.dy)
+      }, 0)
+
+      if (somme + rideDistance < nsteps) {
+        stack[count].push(index++);
+        copyRides.shift();
+      }
     }
-
-    const { ox, oy, dx, dy } = rides[index]
-    const rideDistance = distance(ox, oy, dx, dy)
-    const vehiculeRides = stack[count]
-
-    const somme = vehiculeRides.reduce((sum , rideIndex) => {
-      const tasoeur = rides[rideIndex]
-      return sum + distance(tasoeur.ox, tasoeur.oy, tasoeur.dx, tasoeur.dy)
-    }, 0)
-
-    if (somme + rideDistance < nsteps) {
-      stack[count].push(index++);
-      copyRides.shift();
-    }
+  } catch (e) {
+    console.log(index, count)
   }
 
   return _.map(stack, ridesPerVehicle => {
